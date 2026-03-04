c2c9b1a6be1711de85f30913b947bb0d7c9edd5c
ryleq <semjon.khrushchev@gmail.com>
2026-03-04 20:02:03 +0100

    feat: Added get_config_path file, which gets the path to the app config and caches it

--- src/utils/file_utils.py
+++ src/utils/file_utils.py
@@ -1,5 +1,9 @@
+from functools import cache
 from pathlib import Path
 import platform
+import logging
+
+logger: logging.Logger = logging.getLogger(__name__)
 
 """Here we dump all low-level path resolution functinons and anything regarding paths"""
 
@@ -36,3 +40,19 @@ def get_default_lockfile_path() -> Path:
             / ".local" / "share"
             / "riot-games" / "lockfile"
         )
+      
+@cache  
+def get_config_path() -> Path:
+    """Returns a Path object to the app config json
+
+    Raises:
+        FileNotFoundError: The config file does not exist
+
+    Returns:
+        Path: The path object to the config file
+    """
+    config_path: Path = Path(__file__).resolve().parents[2] / "config.json"
+    if not config_path.exists():
+        logger.error("The config file does not exist, cannot launch properly.")
+        raise FileNotFoundError("The config file does not exist, cannot launch properly.")
+    return config_path
